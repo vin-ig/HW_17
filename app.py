f834c6012cadef26f73729744fc95f6107e4ce80
Harry <vin-ig@yandex.ru>
2022-05-11 22:39:31 +0300

    app.py changed

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-# app.py
-
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restx import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema, fields
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSON_AS_ASCII'] = False
+app.config['JSON_PRETTYPRINT_REGULAR'] = True
 db = SQLAlchemy(app)
 
 
@@ -24,6 +25,7 @@ class Movie(db.Model):
     director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
     director = db.relationship("Director")
 
+
 class Director(db.Model):
     __tablename__ = 'director'
     id = db.Column(db.Integer, primary_key=True)
@@ -36,6 +38,29 @@ class Genre(db.Model):
     name = db.Column(db.String(255))
 
 
+class MovieSchema(Schema):
+    pass
+
+
+class DirectorSchema(Schema):
+    pass
+
+
+class GenreSchema(Schema):
+    pass
+
+
+api = Api(app)
+movie_ns = api.namespace('movies')
+director_ns = api.namespace('directors')
+genre_ns = api.namespace('genres')
+
+
+@movie_ns.route('/')
+class MoviesView(Resource):
+    def get(self):
+        return jsonify(Movie.query.all())
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
