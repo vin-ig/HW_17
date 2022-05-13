ef7660954248f1ca5f657a08962852d195c0a850
Harry <101124180+vin-ig@users.noreply.github.com>
2022-05-13 07:30:05 +0300

    Config RESTX-JSON added

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.config['JSON_AS_ASCII'] = False
+app.config['RESTX_JSON'] = {'ensure_ascii': False, 'indent': 2}
+
 db = SQLAlchemy(app)
 
 
